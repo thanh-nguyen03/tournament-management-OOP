9971d094b534afbcbcee096c539685ec86f7da8b
thanhnguyenduc <nguyenducthanh240903@gmail.com>
2023-10-04 17:10:27 +0700

    add: player screen with crud functionality

--- src/main/java/com/donghochanh/tournamentmanagement/mapper/TableMapping.java
+++ src/main/java/com/donghochanh/tournamentmanagement/mapper/TableMapping.java
@@ -2,14 +2,17 @@ package com.donghochanh.tournamentmanagement.mapper;
 
 import com.donghochanh.tournamentmanagement.entity.Player;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class TableMapping {
-	public List<List<Object>> playerToTable(List<Player> players) {
-		List<List<Object>> table = new ArrayList<>();
-		for (Player player : players) {
-			table.add(new ArrayList<>(List.of(player.getId(), player.getName(), player.getAge(), player.getNationality())));
+	public static Object[][] playerToTable(List<Player> players) {
+		Object[][] table = new Object[players.size()][5];
+		for (int i = 0; i < players.size(); i++) {
+			table[i][0] = i + 1;
+			table[i][1] = players.get(i).getId();
+			table[i][2] = players.get(i).getName();
+			table[i][3] = players.get(i).getAge();
+			table[i][4] = players.get(i).getNationality();
 		}
 		return table;
 	}
